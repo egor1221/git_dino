29d5833f3c0ed27f89be934384422a2619ffa1ae
KOGR <66009682+egor1221@users.noreply.github.com>
2023-12-22 18:33:07 +0300

    111111

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 let pers = document.getElementById('pers')
 let goat = document.getElementById('goat')
 
-document.addEventListener('keydown', jump)
+document.addEventListener('keydown', (event) => {
+    jump()
+})
+
+function jump(){
+    if(pers.classList !== 'jump'){
+        pers.classList.add('jump')
+    }
+    setTimeout(() => {
+        pers.classList.remove('jump')
+    }, 300)
+}
+
+let isAlive = setInterval(() => {
+    let persTop = parseInt(window.getComputedStyle(pers).getPropertyValue('top'))
+    let goatLeft = parseInt(window.getComputedStyle(goat).getPropertyValue('left'))
+
+    if(goatLeft<40 && goatLeft>0 && persTop>=325){
+        alert('ddddddddd')
+    }
+}, 10)
